191c44a7e453068bd975904832bd2bc2097532ad
ark1987s <arkadiy1987@yandex.ru>
2022-10-26 14:20:42 +0300

    new

---
+++ src/Lab01/Ex03.java
@@ -0,0 +1,14 @@
+package Lab01;
+
+public class Ex03 {
+    public static void main(String[] args)
+    {
+        int number;
+        number= 10500;
+
+        float result;
+        result= (number/10)/10;
+
+        System.out.println(result);
+    }
+}
---
+++ src/Lab01/Ex04.java
@@ -0,0 +1,15 @@
+package Lab01;
+
+public class Ex04 {
+    public static void main(String[] args)
+    {
+        float a,b,c;
+        a=3.6f;
+        b=4.1f;
+        c=5.9f;
+
+        System.out.println((float)(a*b*c));
+
+        System.out.println((float)(3.6*4.1*5.9));
+    }
+}
